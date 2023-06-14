788712806ffe863abf256d214c9ac38f7a2070af
Osman <72527925+rosman83@users.noreply.github.com>
2023-06-14 08:46:16 -0500

    Adding image alts

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className='absolute top-0 left-0 w-full h-full backdrop-blur-sm flex items-center justify-center'>
     <div className='py-10 px-6  backdrop-blur-md bg-white/80 rounded-lg flex flex-col items-center justify-center w-fit gap-6'>
       <div className='flex flex-row gap-4 items-center px-3'>
-        <Image src='/logo.png' width={72} height={72} />
+        <Image alt="Baylor College of Medicine" src='/logo.png' width={72} height={72} />
         <h1 className='text-2xl md:text-3xl lg:text-4xl'>Frederick & <br/>Sandulache Lab</h1>
       </div>
       <div className='flex flex-col gap-2 w-full md:mt-4'>
