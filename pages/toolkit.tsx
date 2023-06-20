02423bc28f11494649f3c160b9f08b6d562e9b09
Osman <72527925+rosman83@users.noreply.github.com>
2023-06-20 08:07:12 -0500

    Adding more usage instructions

--- pages/toolkit.tsx
+++ pages/toolkit.tsx
@@ -13,7 +13,14 @@ Tools used for Head and Neck Cancer research at Baylor College of Medicine, with
 
 ## How to Use
 
-Download the latest open source release for your platform [here](https://github.com/rosman83/bcmhnc-toolkit/releases "download"), and run the program installer, application will automatically alert you to updates in the future.
+1. Download the latest open source release for your platform [here](https://github.com/rosman83/bcmhnc-toolkit/releases "download"), and run the program installer, application will automatically alert you to updates in the future.
+
+2. Open the program, navigate to the "Analysis Pipeline" tab and select the folder containing your genetic data files, or drag and drop the folder into the program window. The program is intelligent enough to recursively scan all subfolders for genetic data files. 
+
+3. Assign group letters or numbers to the generated table, ensure only a total of two groups, select the upper group for calculations, a threshold for normalization, and click "Run Analysis". 
+
+Allow a few minutes for the pipeline to run. Once finished, you will be asked to save a compressed file containing the results in CSV and GCT formats, along with a report file on problematic gene edge cases encountered with appropriate context.
+
 
 ## Brief Pipeline Summary
 
@@ -21,7 +28,7 @@ Input genetic result files from assorted folder and assign groups to obtain comb
 
 Pipeline consists of recursively scanning all input files to support various genetic data folder schemas, asking user to label testing groups.Data is filtered for existing genes based on lookup list, and transformed into linear and log space data.
 
-Further normalization is carried out in preparation for Benjamini - Hochberg Procedure.Procedure used calculating fold changes and p values for significant genes via a user defined threshold.
+Further normalization is carried out using Benjamini - Hochberg Procedure. Fold changes and p values for significant genes via a user defined threshold are outputted.
 `;
 
 function Toolkit() {
